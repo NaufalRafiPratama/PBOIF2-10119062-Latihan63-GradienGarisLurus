66a8834c044eac620ef4e6d9d40a48f7ddc9b6dc
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-11-29 06:43:50 +0700

    Add files via upload

---
+++ PBOIF2-10119062-Latihan63-GradienGarisLurus/src/pboif2/pkg10119062/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119062.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Progam : program ini berisi program untuk menampilkan
+ * gradien garis lurus
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
